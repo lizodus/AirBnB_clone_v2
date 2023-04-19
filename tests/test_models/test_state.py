da4b4a184530c58a5932261626beb0b6c07c0851
Elizabeth K. G-S <81371428+lizodus@users.noreply.github.com>
2023-04-20 02:19:09 +0400

    update

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-""" Module that tests file: state.py """
+""" """
+import os
 
-from tests.test_models.test_base_model import test_basemodel
+from tests.test_models.test_base_model import TestBasemodel
 from models.state import State
 
 
-class test_state(test_basemodel):
-    """ Class that tests the state.py"""
-
+class TestState(TestBasemodel):
+    """Represents the tests for the State model."""
     def __init__(self, *args, **kwargs):
-        """ Constructor for the test_state class """
+        """Initializes the test class."""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ More testing of state name"""
+        """Tests the type of name."""
         new = self.value()
-        self.assertNotEqual(type(new.name), str)
+        self.assertEqual(
+            type(new.name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
