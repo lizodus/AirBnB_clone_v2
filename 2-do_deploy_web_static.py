6d88041013b2cd60671ec1517c5e39a00057ac39
Elizabeth K. G-S <81371428+lizodus@users.noreply.github.com>
2023-05-05 00:37:26 +0400

    still fab

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["54.208.46.187", "	54.86.107.79"]
+env.hosts = ["44.210.150.159", "35.173.47.15"]
 env.user = "ubuntu"
 
 
